9fd8c9f8e18604d90e4df0cb36947859b26f300a
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-03-17 19:10:02 +0530

    backend vercel

--- Backend/index.js
+++ Backend/index.js
@@ -5,11 +5,17 @@ const cors = require('cors');
 const port = 3000;
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://local-link-gistathon.vercel.app/"],
+    methods:["POST","GET","PUT"],
+    credentials:true
+  }
+));
 app.use(express.json())
 
 app.use("/api/v1", rootRouter);
 
-app.listen(port, () => {
-  console.log(`connected to port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`connected to port ${port}`);
+// });
